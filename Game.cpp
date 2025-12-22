1cc828e236bfbbb6c5a2be3e5b04a402638e2ab8
Andezion <245122@edu.p.lodz.pl>
2025-12-22 13:43:37 +0100

    fixed camera

--- Game.cpp
+++ Game.cpp
@@ -332,8 +332,6 @@ void Game::Draw()
         DrawText("- Right/Left to move", 40, 40, 10, DARKGRAY);
         DrawText("- Space to jump", 40, 60, 10, DARKGRAY);
         DrawText("- Mouse Wheel to Zoom in-out, R to reset zoom", 40, 80, 10, DARKGRAY);
-        DrawText("- C to change camera mode", 40, 100, 10, DARKGRAY);
-        DrawText("Current camera mode:", 20, 120, 10, BLACK);
 
     EndDrawing();
 }
