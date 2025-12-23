caffd3231efedfea6ca11108519ed5360d742f21
Andezion <245122@edu.p.lodz.pl>
2025-12-23 23:43:54 +0100

    refactor

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,91 @@
 #include "Player.h"
+#include "Game.h"
+#include "raylib.h"
+
+#include <cmath>
+
+#define G 450
+#define PLAYER_JUMP_SPD 450.0f
+#define PLAYER_HOR_SPD 300.0f
+
+Player::Player()
+	: position{100.0f, 500.0f}, speed(0.0f), canJump(false)
+{
+}
+
+void Player::Update(float delta, const std::vector<EnvItem>& envItems)
+{
+	constexpr float halfWidth = 10.0f;
+	constexpr float fullHeight = 60.0f;
+
+	if (IsKeyDown(KEY_A))
+	{
+		position.x -= PLAYER_HOR_SPD * delta;
+	}
+	if (IsKeyDown(KEY_D))
+	{
+		position.x += PLAYER_HOR_SPD * delta;
+	}
+
+	if (IsKeyPressed(KEY_W) && canJump)
+	{
+		speed = -PLAYER_JUMP_SPD;
+		canJump = false;
+	}
+
+	speed += G * delta;
+	position.y += speed * delta;
+
+	Rectangle playerRect = {
+		position.x - halfWidth,
+		position.y - fullHeight,
+		halfWidth * 2.0f,
+		fullHeight
+	};
+
+	bool grounded = false;
+
+	for (const auto &[rect, blocking, color] : envItems)
+	{
+		if (!blocking) continue;
+
+		if (const Rectangle &rectangle = rectangle; CheckCollisionRecs(rectangle, playerRect))
+		{
+			const float overlapLeft = (playerRect.x + playerRect.width) - rectangle.x;
+			const float overlapRight = (rectangle.x + rectangle.width) - playerRect.x;
+			const float overlapTop = (playerRect.y + playerRect.height) - rectangle.y;
+			const float overlapBottom = (rectangle.y + rectangle.height) - playerRect.y;
+
+			const float minOverlap = fminf(
+				fminf(overlapLeft, overlapRight),
+				fminf(overlapTop, overlapBottom)
+			);
+
+			if (minOverlap == overlapTop)
+			{
+				position.y = rectangle.y;
+				speed = 0.0f;
+
+				grounded = true;
+			}
+			else if (minOverlap == overlapBottom)
+			{
+				position.y = rectangle.y + rectangle.height + fullHeight;
+				speed = 0.0f;
+			}
+			else if (minOverlap == overlapLeft)
+			{
+				position.x = rectangle.x - halfWidth;
+			}
+			else
+			{
+				position.x = rectangle.x + rectangle.width + halfWidth;
+			}
+
+			playerRect.x = position.x - halfWidth;
+			playerRect.y = position.y - fullHeight;
+		}
+	}
+
+	canJump = grounded;
+}
