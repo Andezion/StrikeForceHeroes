8fabe834f1ac4a98999d2f2d8a1d95003d570211
Andezion <245122@edu.p.lodz.pl>
2025-12-23 23:44:04 +0100

    refactor

--- Player.h
+++ Player.h
@@ -1,10 +1,19 @@
 #pragma once
 #include "raylib.h"
+#include <vector>
 
+struct EnvItem; 
 
 class Player
 {
+public:
+	Player();
 
+	Vector2 position;
+	float speed;
+	bool canJump;
+
+	void Update(float delta, const std::vector<EnvItem>& envItems);
 };
 
 
