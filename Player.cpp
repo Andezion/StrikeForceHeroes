b7b107f892ef87b31c406e04923ec195cb679f19
Andezion <245122@edu.p.lodz.pl>
2025-12-23 23:20:32 +0100

    fixed it

---
+++ Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vdoro on 23.12.2025.
+//
+
+#include "Player.h"
---
+++ Player.h
@@ -0,0 +1,16 @@
+//
+// Created by vdoro on 23.12.2025.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+
+class Player {
+
+};
+
+
+
+#endif //PLAYER_H
