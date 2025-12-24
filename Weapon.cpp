f98601f936f210678f2eb2fc356799eacbba10db
Andezion <245122@edu.p.lodz.pl>
2025-12-24 16:30:07 +0100

    added cool stuff

---
+++ Weapon.cpp
@@ -0,0 +1,29 @@
+#include "Weapon.h"
+#include "raylib.h"
+
+#include <cmath>
+
+Weapon::Weapon(const float length, const float thickness, const Color color)
+    : anchor{0.0f, 0.0f}, length(length), thickness(thickness), rotationDegrees(0.0f), color(color)
+{
+}
+
+void Weapon::Update(const Vector2 &anchorPos, const Vector2 &targetPos)
+{
+    anchor = anchorPos;
+
+    const float dx = targetPos.x - anchor.x;
+    const float dy = targetPos.y - anchor.y;
+
+    rotationDegrees = atan2f(dy, dx) * 180.0f / PI;
+}
+
+void Weapon::Draw() const
+{
+    const Rectangle rec = { anchor.x, anchor.y - thickness * 0.5f, length, thickness };
+    const Vector2 origin = { 0.0f, thickness * 0.5f };
+
+    DrawRectanglePro(rec, origin, rotationDegrees, color);
+
+    DrawCircleV(anchor, thickness * 0.6f, DARKGRAY);
+}
