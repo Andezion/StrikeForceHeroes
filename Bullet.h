c458aba8468ff726de1898338de40debd56196a1
Andezion <245122@edu.p.lodz.pl>
2025-12-25 23:28:41 +0100

    cleaned up some code

--- Bullet.h
+++ Bullet.h
@@ -6,10 +6,11 @@
 struct EnvItem;
 class Particle;
 
+
 class Bullet
 {
 public:
-    Bullet(const Vector2 &startPos, const Vector2 &aimTarget, float speed = 900.0f, float spreadRadius = 0.0f);
+    Bullet(const Vector2 &startPos, const Vector2 &initialVel, float spreadRadius = 0.0f);
 
     bool Update(float delta, const std::vector<EnvItem> &envItems, std::vector<Particle> &outParticles);
     void Draw() const;
@@ -19,7 +20,6 @@ public:
 private:
     Vector2 pos{};
     Vector2 vel{};
-    float speed{};
     float radius = 4.0f;
     bool active = true;
     Color color = DARKGRAY;
