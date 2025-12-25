d70a11172f0c2ce517448cb8619aa50c0e1e9635
Andezion <245122@edu.p.lodz.pl>
2025-12-25 23:27:39 +0100

    cleaned up some code

--- Weapon.cpp
+++ Weapon.cpp
@@ -4,12 +4,30 @@
 #include "Particle.h"
 
 #include <cmath>
+#include <random>
+#include <ctime>
+
+#include "raymath.h"
+
+static float RandomFloatW(const float a, const float b)
+{
+    static std::mt19937 rng(static_cast<unsigned>(time(nullptr)));
+    std::uniform_real_distribution<float> dist(a, b);
+    return dist(rng);
+}
+
+static Vector2 RandomPointInCircleW(const Vector2 &center, const float r)
+{
+    const float t = RandomFloatW(0.0f, 2.0f * PI);
+    const float u = RandomFloatW(0.0f, 1.0f);
+
+    const float rad = r * sqrtf(u);
+    return Vector2{ center.x + cosf(t) * rad, center.y + sinf(t) * rad };
+}
 
 Weapon::Weapon(const float length, const float thickness, const float bulletSpeed, const float cooldown)
     : anchor{0.0f, 0.0f}, length(length), thickness(thickness), rotationDegrees(0.0f),
-      cooldown(cooldown), cooldownTimer(0.0f), bulletSpeed(bulletSpeed), bullets()
-{
-}
+      cooldown(cooldown), cooldownTimer(0.0f), bulletSpeed(bulletSpeed), bullets() {}
 
 void Weapon::Update(const float delta, const Vector2 &anchorPos, const Vector2 &targetPos,
     const std::vector<EnvItem> &envItems, std::vector<Particle> &outParticles, const float spreadRadius)
@@ -31,7 +49,25 @@ void Weapon::Update(const float delta, const Vector2 &anchorPos, const Vector2 &
         const float rad = rotationDegrees * PI / 180.0f;
         const Vector2 endPos = { anchor.x + cosf(rad) * length, anchor.y + sinf(rad) * length };
 
-        bullets.emplace_back(endPos, targetPos, bulletSpeed, spreadRadius);
+        Vector2 target = targetPos;
+        if (spreadRadius > 0.0001f)
+        {
+            target = RandomPointInCircleW(targetPos, spreadRadius);
+        }
+
+        const Vector2 dir = Vector2Subtract(target, endPos);
+        const float lenDir = Vector2Length(dir);
+        Vector2 vel;
+        if (lenDir > 0.0001f)
+        {
+            vel = Vector2Scale(dir, 1.0f / lenDir * bulletSpeed);
+        }
+        else
+        {
+            vel = { bulletSpeed, 0 };
+        }
+
+        bullets.emplace_back(endPos, vel, 0.0f);
         cooldownTimer = cooldown;
     }
 
