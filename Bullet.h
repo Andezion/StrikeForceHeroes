c8c62521b3d151ea5b6383e0c43b8d229016e0cb
Andezion <245122@edu.p.lodz.pl>
2025-12-25 20:36:38 +0100

    added bullet

---
+++ Bullet.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "raylib.h"
+#include <vector>
+
+struct EnvItem; 
+
+class Bullet
+{
+public:
+    Bullet(const Vector2 &startPos, const Vector2 &aimTarget, float speed = 900.0f, float spreadRadius = 0.0f);
+
+    bool Update(float delta, const std::vector<EnvItem> &envItems);
+    void Draw() const;
+
+    bool IsActive() const { return active; }
+
+private:
+    Vector2 pos{};
+    Vector2 vel{};
+    float speed{};
+    float radius = 4.0f;
+    bool active = true;
+    Color color = YELLOW;
+};
