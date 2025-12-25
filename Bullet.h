66396d3765bc4f491745430213a3a84bc3b4f593
Andezion <245122@edu.p.lodz.pl>
2025-12-25 21:30:19 +0100

    added cool stuff

--- Bullet.h
+++ Bullet.h
@@ -3,20 +3,18 @@
 #include "raylib.h"
 #include <vector>
 
-struct EnvItem; 
+struct EnvItem;
+class Particle;
 
 class Bullet
 {
 public:
     Bullet(const Vector2 &startPos, const Vector2 &aimTarget, float speed = 900.0f, float spreadRadius = 0.0f);
 
-    bool Update(float delta, const std::vector<EnvItem> &envItems);
+    bool Update(float delta, const std::vector<EnvItem> &envItems, std::vector<Particle> &outParticles);
     void Draw() const;
 
-    [[nodiscard]] bool IsActive() const
-    {
-        return active;
-    }
+    [[nodiscard]] bool IsActive() const { return active; }
 
 private:
     Vector2 pos{};
@@ -24,5 +22,5 @@ private:
     float speed{};
     float radius = 4.0f;
     bool active = true;
-    Color color = YELLOW;
+    Color color = DARKGRAY;
 };
