691fa55b7aab10146c1b3a4118204dc61fe3ff41
Andezion <245122@edu.p.lodz.pl>
2025-12-25 23:28:34 +0100

    cleaned up some code

--- Bullet.cpp
+++ Bullet.cpp
@@ -14,35 +14,8 @@ static float RandomFloat(const float a, const float b)
     std::uniform_real_distribution dist(a, b);
     return dist(rng);
 }
-
-static Vector2 RandomPointInCircle(const Vector2 &center, const float r)
-{
-    const float t = RandomFloat(0.0f, 2.0f * PI);
-    const float u = RandomFloat(0.0f, 1.0f);
-
-    const float rad = r * sqrtf(u);
-    return Vector2{ center.x + cosf(t)*rad, center.y + sinf(t)*rad };
-}
-
-Bullet::Bullet(const Vector2 &startPos, const Vector2 &aimTarget, const float speed, const float spreadRadius)
-    : pos(startPos), speed(speed)
-{
-    Vector2 target = aimTarget;
-    if (spreadRadius > 0.0001f)
-    {
-        target = RandomPointInCircle(aimTarget, spreadRadius);
-    }
-
-    const Vector2 dir = Vector2Subtract(target, startPos);
-    if (const float len = Vector2Length(dir); len > 0.0001f)
-    {
-        vel = Vector2Scale(dir, 1.0f / len * speed);
-    }
-    else
-    {
-        vel = { speed, 0 };
-    }
-}
+Bullet::Bullet(const Vector2 &startPos, const Vector2 &initialVel, const float)
+    : pos(startPos), vel(initialVel) {}
 
 bool Bullet::Update(const float delta, const std::vector<EnvItem> &envItems, std::vector<Particle> &outParticles)
 {
@@ -52,6 +25,11 @@ bool Bullet::Update(const float delta, const std::vector<EnvItem> &envItems, std
     }
 
     pos = Vector2Add(pos, Vector2Scale(vel, delta));
+
+    {
+        const Vector2 pVel = Vector2Scale(vel, -0.02f);
+        outParticles.emplace_back(pos, pVel, RandomFloat(0.12f, 0.25f), RandomFloat(0.6f, 1.2f), WHITE);
+    }
 
     for (const auto &[rect, blocking, color] : envItems)
     {
