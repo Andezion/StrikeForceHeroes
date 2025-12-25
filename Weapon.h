79ec6314be175530389df5e1f49759e0c7cbdbf8
Andezion <245122@edu.p.lodz.pl>
2025-12-25 20:49:02 +0100

    added new stuff

--- Weapon.h
+++ Weapon.h
@@ -1,12 +1,17 @@
 #pragma once
 #include "raylib.h"
+#include <vector>
+
+struct EnvItem;
+
+#include "Bullet.h"
 
 class Weapon
 {
 public:
-    explicit Weapon(float length = 50.0f, float thickness = 6.0f);
+    explicit Weapon(float length = 50.0f, float thickness = 6.0f, float bulletSpeed = 900.0f, float cooldown = 1.0f);
 
-    void Update(const Vector2 &anchorPos, const Vector2 &targetPos);
+    void Update(float delta, const Vector2 &anchorPos, const Vector2 &targetPos, const std::vector<EnvItem> &envItems, float spreadRadius = 0.0f);
     void Draw() const;
 
 private:
@@ -14,4 +19,10 @@ private:
     float length;
     float thickness;
     float rotationDegrees;
+
+    float cooldown; 
+    float cooldownTimer;
+    float bulletSpeed;
+
+    std::vector<Bullet> bullets;
 };
