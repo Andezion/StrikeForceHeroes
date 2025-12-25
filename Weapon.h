308ffbc193113fa2b650b0cb9639c28c2dcc4dba
Andezion <245122@edu.p.lodz.pl>
2025-12-25 23:32:13 +0100

    fixed

--- Weapon.h
+++ Weapon.h
@@ -12,7 +12,7 @@ public:
     explicit Weapon(float length = 50.0f, float thickness = 6.0f, float bulletSpeed = 1800.0f, float cooldown = 1.0f);
 
     void Update(float delta, const Vector2 &anchorPos, const Vector2 &targetPos,
-        const std::vector<EnvItem> &envItems, std::vector<class Particle> &outParticles, float spreadRadius = 0.0f);
+        const std::vector<EnvItem> &envItems, std::vector<Particle> &outParticles, float spreadRadius = 0.0f);
     void Draw() const;
 
     [[nodiscard]] bool IsCooling() const;
