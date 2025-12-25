574806dba5ee601ee253888d091f4b15893ae5f4
Andezion <245122@edu.p.lodz.pl>
2025-12-25 20:53:44 +0100

    added new stuff

--- Weapon.h
+++ Weapon.h
@@ -11,7 +11,8 @@ class Weapon
 public:
     explicit Weapon(float length = 50.0f, float thickness = 6.0f, float bulletSpeed = 900.0f, float cooldown = 1.0f);
 
-    void Update(float delta, const Vector2 &anchorPos, const Vector2 &targetPos, const std::vector<EnvItem> &envItems, float spreadRadius = 0.0f);
+    void Update(float delta, const Vector2 &anchorPos, const Vector2 &targetPos,
+        const std::vector<EnvItem> &envItems, float spreadRadius = 0.0f);
     void Draw() const;
 
 private:
