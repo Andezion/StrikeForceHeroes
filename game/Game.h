83c0ff6dba4e980be1a2e4dc6b06394c7bba91d6
Andezion <245122@edu.p.lodz.pl>
2026-03-28 14:34:14 +0100

    add: rebased stuff

---
+++ game/Game.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "raylib.h"
+#include <vector>
+
+struct EnvItem {
+    Rectangle rect;
+    int blocking;
+    Color color;
+};
+
+#include "game/Player.h"
+#include "shoot/Aim.h"
+#include "shoot/Particle.h"
+#include "network/NetworkClient.h"
+
+#include <unordered_map>
+#include <cstdint>
+
+class Game
+{
+public:
+    Game(int screenWidth, int screenHeight);
+    ~Game();
+
+    void Update(float delta);
+    void Draw();
+
+private:
+    int screenWidth;
+    int screenHeight;
+
+    Player player{};
+    std::vector<EnvItem> envItems;
+    Camera2D camera{};
+
+    Aim aim{ Aim::Type::Default };
+
+    std::vector<Particle> particles;
+
+    using CameraUpdater = void(*)(Camera2D*, Player*, EnvItem*, int, float, float, float);
+    std::vector<CameraUpdater> cameraUpdaters;
+    int cameraOption = 0;
+
+    void InitScene();
+
+    NetworkClient netClient;
+    uint32_t clientId = 0;
+    std::unordered_map<uint32_t, Vector2> remotePlayers;
+
+    float sendTimer = 0.0f;
+    static inline constexpr float SEND_PERIOD = 0.1f;
+};
